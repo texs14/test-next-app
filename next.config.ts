c6bedfd5f38621b92b4d4ab988e23c9dbc9057ba
Ruslan M <mihailovrv@hopper-it.ru>
2025-06-10 01:27:28 +0700

    feature improveExercise

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["firebasestorage.googleapis.com", 'i.pravatar.cc'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/**',
+      },
+    ]
   },
 };
 
